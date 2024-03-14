2063bb6f052de0c01c275bb506b01ecb18af1bc0
parklego <hbd9425@gmail.com>
2024-03-15 00:55:08 +0900

    home 화면 (next/image)

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import main from "/public/main.png";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Elit voluptate minim occaecat adipisicing amet cillum dolore.
+        </h1>
+        <p className={styles.desc}>
+          Qui eu Lorem eiusmod eiusmod eiusmod enim non dolore aliqua ad.
+        </p>
+        <button className={styles.button}>
+          Nulla nostrud cillum do est excepteur cillum labore esse.
+        </button>
+      </div>
+      <div className={styles.item}>
+        <Image className={styles.img} src={main} alt="main image" />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>2024 parklego. All rights reseved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          className={styles.icon}
+          src="/facebook.png"
+          width={20}
+          height={20}
+          alt="sns account"
+        />
+        <Image
+          className={styles.icon}
+          src="/instagram.png"
+          width={20}
+          height={20}
+          alt="sns account"
+        />
+        <Image
+          className={styles.icon}
+          src="/twitter.png"
+          width={20}
+          height={20}
+          alt="sns account"
+        />
+        <Image
+          className={styles.icon}
+          src="/youtube.png"
+          width={20}
+          height={20}
+          alt="sns account"
+        />
+      </div>
     </div>
   );
 };
