27c928054b6a2754c48b142de9074e9c5d0730bb
parklego <hbd9425@gmail.com>
2024-03-15 04:08:00 +0900

    content 페이지 추가

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Irure dolore ipsum proident culpa anim.</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src={"/contact.png"}
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.input}
+            placeholder="message"
+            cols={30}
+            rows={10}
+          />
+
+          <Button url="#" text="send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
