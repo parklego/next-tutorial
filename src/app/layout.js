0f0a63a88f83fa921f0619f95833176a83803441
parklego <hbd9425@gmail.com>
2024-03-22 18:16:28 +0900

    context 경로수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
-import { ThemeProvider } from "@/context/ThemeContext";
+import { ThemeProvider } from "@/app/context/ThemeContext";
 
 export const metadata = {
   title: "Create Next App",
