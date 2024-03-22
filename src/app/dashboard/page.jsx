2423a2b4128c5e9866d036484793141c252efaee
parklego <hbd9425@gmail.com>
2024-03-22 18:16:52 +0900

    swr test

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,11 @@
+"use client";
+import { fetcher } from "@/utils/fetcher";
 import React from "react";
+import useSWR from "swr";
 
 const Dashboard = () => {
+  const { data, isLoading, error } = useSWR("", fetcher);
+
   return <div>Dashboard</div>;
 };
 
