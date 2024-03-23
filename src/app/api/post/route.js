5f5889576fedc6f047b2171b3542bcd7c2b3b26e
parklego <hbd9425@gmail.com>
2024-03-23 12:35:50 +0900

    dashboard 페이지

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -3,10 +3,15 @@ import connect from "@/utils/db";
 import Post from "@/models/Post";
 
 export const GET = async (req, res, next) => {
+  const url = new URL(req.url);
+
+  const username = url.searchParams.get("username");
+
   try {
     await connect();
 
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
+
     return new NextResponse(JSON.stringify(posts), {
       status: 200,
     });
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,12 +4,17 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import useSWR from "swr";
+import Image from "next/image";
 
 const Dashboard = () => {
-  const { data, isLoading, error } = useSWR("", fetcher);
   const router = useRouter();
   const session = useSession();
 
+  const { data, isLoading, error } = useSWR(
+    `/api/post?username=${session?.data?.user?.name}`,
+    fetcher
+  );
+  console.log("dashboard data", data);
   if (session.status === "loading") {
     return <p>loading..</p>;
   }
@@ -19,7 +24,27 @@ const Dashboard = () => {
   }
 
   if (session.status === "authenticated") {
-    return <div>{`Hi ${session.data.user.name}`}</div>;
+    return (
+      <div>
+        <p
+          style={{ fontSize: "30px", marginBottom: "30px" }}
+        >{`Hi ${session.data.user.name}`}</p>
+        {data?.length === 0 && <p>현재 등록된 게시물이 없습니다.</p>}
+        {data?.map((item) => (
+          <div key={item._id}>
+            <div>
+              <div>
+                <h1>{item.title}</h1>
+                <p>{item.desc}</p>
+              </div>
+              <div>
+                <Image width={250} height={250} src={item.image} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 };
 
